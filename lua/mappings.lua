bef83a238c71f267cf3a7ab780015375b81b4bd9
surattinon <surattinon.basu@gmail.com>
2024-06-01 11:19:49 +0700

    Update 0.0.1

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- CUSTOM TERM
 map({ "n", "t" }, "<A-g>", function()
@@ -28,5 +29,3 @@ map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", {})
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", {})
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", {})
 map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", {})
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,17 @@
 return {
   -- ALPHA DASHBOARD
-  {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    priority = 1000,
-    lazy = true,
-    opts = {
-      position = "center",
-    },
-    config = function()
-      require "configs.dashboard"
-    end,
-  },
+  -- {
+  --   "goolord/alpha-nvim",
+  --   event = "VimEnter",
+  --   priority = 1000,
+  --   lazy = true,
+  --   opts = {
+  --     position = "center",
+  --   },
+  --   config = function()
+  --     require "configs.dashboard"
+  --   end,
+  -- },
 
   -- NOCIE
   {
@@ -22,6 +22,14 @@ return {
     },
     config = function()
       require "configs.noice"
+    end,
+  },
+
+  -- SMOOTH SCROLL
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup { mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>" } }
     end,
   },
 }
